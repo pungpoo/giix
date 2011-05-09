ff3e108284b2ed17cead569da86a2259133f71df
rodrigo.coelho@gmail.com <rodrigo.coelho@gmail.com@f41ef23b-66d8-86e7-7931-d4dca6908580>
2011-05-09 18:13:06 +0000

    1.6 release.

---
+++ tags/1.6/giix-components/GxActiveForm.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * GxActiveForm class file.
+ *
+ * @author Rodrigo Coelho <rodrigo@giix.org>
+ * @link http://giix.org/
+ * @copyright Copyright &copy; 2010-2011 Rodrigo Coelho
+ * @license http://giix.org/license/ New BSD License
+ */
+
+/**
+ * GxActiveForm provides forms with additional features.
+ *
+ * @author Rodrigo Coelho <rodrigo@giix.org>
+ * @since 1.0
+ */
+class GxActiveForm extends CActiveForm {
+
+	/**
+	 * Renders a checkbox list for a model attribute.
+	 * This method is a wrapper of {@link GxHtml::activeCheckBoxList}.
+	 * Overrides and based on {@link CActiveForm::checkBoxList}.
+	 * Changes: uses GxHtml.
+	 * @see {@link CActiveForm::checkBoxList} for more information.
+	 * @param CModel $model The data model.
+	 * @param string $attribute The attribute.
+	 * @param array $data Value-label pairs used to generate the check box list.
+	 * @param array $htmlOptions Addtional HTML options.
+	 * @return string The generated check box list.
+	 */
+	public function checkBoxList($model, $attribute, $data, $htmlOptions = array()) {
+		return GxHtml::activeCheckBoxList($model, $attribute, $data, $htmlOptions);
+	}
+
+}

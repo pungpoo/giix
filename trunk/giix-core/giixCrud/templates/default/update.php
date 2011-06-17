153a985fcca947543692896ffa440c8b06b8d01e
rodrigo.coelho@gmail.com <rodrigo.coelho@gmail.com@f41ef23b-66d8-86e7-7931-d4dca6908580>
2011-06-17 05:22:28 +0000

    Enh: Added support to active record labels to the CRUD and model templates.

--- trunk/giix-core/giixCrud/templates/default/update.php
+++ trunk/giix-core/giixCrud/templates/default/update.php
@@ -5,24 +5,23 @@
  */
 ?>
 <?php
-echo "<?php\n";
-$label = $this->pluralize($this->class2name($this->modelClass));
-echo "\$this->breadcrumbs = array(
-	'$label' => array('index'),
+echo "<?php\n
+\$this->breadcrumbs = array(
+	\$model->label(2) => array('index'),
 	GxHtml::valueEx(\$model) => array('view', 'id' => GxActiveRecord::extractPkValue(\$model, true)),
 	Yii::t('app', 'Update'),
 );\n";
 ?>
 
 $this->menu = array(
-	array('label' => Yii::t('app', 'List') . ' <?php echo $this->modelClass; ?>', 'url'=>array('index')),
-	array('label' => Yii::t('app', 'Create') . ' <?php echo $this->modelClass; ?>', 'url'=>array('create')),
-	array('label' => Yii::t('app', 'View') . ' <?php echo $this->modelClass; ?>', 'url'=>array('view', 'id' => GxActiveRecord::extractPkValue($model, true))),
-	array('label' => Yii::t('app', 'Manage') . ' <?php echo $this->modelClass; ?>', 'url'=>array('admin')),
+	array('label' => Yii::t('app', 'List') . ' ' . $model->label(2), 'url'=>array('index')),
+	array('label' => Yii::t('app', 'Create') . ' ' . $model->label(), 'url'=>array('create')),
+	array('label' => Yii::t('app', 'View') . ' ' . $model->label(), 'url'=>array('view', 'id' => GxActiveRecord::extractPkValue($model, true))),
+	array('label' => Yii::t('app', 'Manage') . ' ' . $model->label(2), 'url'=>array('admin')),
 );
 ?>
 
-<h1><?php echo '<?php'; ?> echo Yii::t('app', 'Update'); <?php echo '?>'; ?> <?php echo $this->modelClass . " #<?php echo GxHtml::encode(GxHtml::valueEx(\$model)); ?>"; ?></h1>
+<h1><?php echo '<?php'; ?> echo Yii::t('app', 'Update') . ' ' . GxHtml::encode($model->label()) . ' ' . GxHtml::encode(GxHtml::valueEx($model)); ?></h1>
 
 <?php echo "<?php\n"; ?>
 $this->renderPartial('_form', array(
